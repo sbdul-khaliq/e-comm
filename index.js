8bb993c44c183de0f5e5ddabe5e01b6be9d6d432
AK <azkhaliq37@gmail.com>
2024-07-27 20:57:51 +0500

    update bacend by AK

--- index.js
+++ index.js
@@ -2,23 +2,40 @@ const express = require('express');
 const cors = require('cors');
 require('./db/config');
 const User = require('./db/user');
+const Product = require('./db/product');
 const app = express();
 
 app.use(cors());  // Add this line to enable CORS
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
-    console.log('Request received:', req.body);  // Log the request body for debugging
     let user = new User(req.body);
-    try {
-        let result = await user.save();
-        console.log('User saved:', result);  // Log the result to verify user is saved
-        res.send(result);
-    } catch (error) {
-        console.error('Error saving user:', error);  // Log any errors encountered
-        res.status(500).send('Error saving user');
-    }
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    res.send(result);
 });
+
+
+app.post('/login', async (req,resp)=>{
+    if(req.body.password && req.body.email){
+    let user = await User.findOne(req.body).select("-password");
+    if (user) {
+        resp.send(user);
+    } else {
+        resp.status(404).send('User Not Found');
+    }}else{
+        resp.status(404).send('User Not Found');
+    }
+}) 
+
+
+app.post('/add-product', async (req, resp) => {
+        let product = new Product(req.body);
+        let result = await product.save();
+        resp.send(result);
+});
+
 
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
